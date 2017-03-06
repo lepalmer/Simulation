0ae7aefd683775f56c1c448b387741ae56d6272b
Regina Caputo <regina.caputo@nasa.gov>
2017-03-06 14:37:42 -0500

    adding to create source

--- MEGAlib/source/create_source_file.py
+++ MEGAlib/source/create_source_file.py
@@ -18,17 +18,18 @@ geofile= '$BURSTCUBEPATH/Simulation/MEGAlib/BurstCube_1Cube.geo.setup'
 OneBeam= 'FarFieldPointSource'
 
 #define your energies and angles 
-Log_E=[1,1.2,1.5,1.7,2,2.2,2.5,2.7,3,3.2,3.5,3.7,4]
-angles  =[0]
-#Log_E=[2]
-#angles  =[0,25.8,36.9,45.6,53.1,60]
+#Log_E=[1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2.0,2.1,2.2,2.3,2.4,2.5,2.6,2.7,2.8,2.9,3.0,3.1,3.2,3.3,3.4,3.5,3.6,3.7,3.8,3.9,4.0]
+#angles  =[0]
+Log_E=[2]
+angles  =[0.,5.,10.,15.,20.,25.,30.,35.,40.,45.,50.,55.,60.,65.]
 
 #gives the cosTheta array
 def ang2cos(allAng):
    ang =[]
    for i in allAng:
-      a= round(cos(math.radians(i)),1)
+      a= round(cos(math.radians(i)),4)
       ang.append(a) 
+      #print ang
    return ang
 
 #in keV [316,501,1000,1585, ... ]
@@ -50,8 +51,8 @@ for myene in energies:
       #print (geofile, OneBeam, myene, cosTh, OneBeam, ang, myene)
       
       #this is just a long string, with all the raws of the .source file, and the energies/angles values
-      string= "# An example run for Cosima \n# This was created with the python wrapper --> create_source_file.py <--\n\nVersion          1 \nGeometry         %s // Update this to your path \nCheckForOverlaps 1000 0.01 \nPhysicsListEM    Livermore \n\nStoreCalibrate                 true\nStoreSimulationInfo            true\nStoreOnlyEventsWithEnergyLoss  true  // Only relevant if no trigger criteria is given! \nDiscretizeHits                 true \n\nRun FFPS \nFFPS.FileName              %s_%.3fkeV_Cos%.1f \nFFPS.NTriggers             10000 \n\n\nFFPS.Source One \nOne.ParticleType        1 \nOne.Beam                %s  %.1f 0 \nOne.Spectrum            Mono  %i\nOne.Flux                1000.0 "%(geofile, OneBeam, myene, cosTh, OneBeam, ang, myene)
-      source_file='%s_%.3fkeV_Cos%.1f.source'%(OneBeam,myene,cosTh)
+      string= "# An example run for Cosima \n# This was created with the python wrapper --> create_source_file.py <--\n\nVersion          1 \nGeometry         %s // Update this to your path \nCheckForOverlaps 1000 0.01 \nPhysicsListEM    Livermore \n\nStoreCalibrate                 true\nStoreSimulationInfo            true\nStoreOnlyEventsWithEnergyLoss  true  // Only relevant if no trigger criteria is given! \nDiscretizeHits                 true \n\nRun FFPS \nFFPS.FileName              %s_%.3fkeV_Cos%.3f \nFFPS.NTriggers             10000 \n\n\nFFPS.Source One \nOne.ParticleType        1 \nOne.Beam                %s  %.1f 0 \nOne.Spectrum            Mono  %i\nOne.Flux                1000.0 "%(geofile, OneBeam, myene, cosTh, OneBeam, ang, myene)
+      source_file='%s_%.3fkeV_Cos%.3f.source'%(OneBeam,myene,cosTh)
       sf=open(source_file,'w')
       sf.write(string)
       sf.close()
