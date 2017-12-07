76ae837363ea777fb8efb7470b2e5ac687352be4
Jeremy S. Perkins <jeremyshane@gmail.com>
2017-12-07 16:12:53 -0500

    begining of Eres function.

--- MEGAlib/bin/bcSim.py
+++ MEGAlib/bin/bcSim.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import numpy as np
+
 class bcSim:
 
     def __init__(self, simFile, sourceFile):
@@ -9,6 +11,7 @@ class bcSim:
         self.simFile = simFile
         self.srcFile = sourceFile
 
+        print("Loading " + self.simFile)
         self.simDict = self.fileToDict(simFile,'#',None)
         self.srcDict = self.fileToDict(sourceFile,'#',None)
         self.geoDict = self.fileToDict(self.srcDict['Geometry'][0],'//',None)
@@ -23,8 +26,17 @@ class bcSim:
         with open(filename) as f:
                 for line in f:
                     if commentString not in line:
-                        lineContents = line.split()
+                        if ';' in line:
+                            lineContents = line.split(';')
+                        else:
+                            lineContents = line.split()
                         if len(lineContents) > 1:
+                            if lineContents[0] in megaDict:
+                                if ';' in line:
+                                    megaDict[lineContents[0]].append(lineContents[1:])
+                                else:
+                                    megaDict[lineContents[0]].append(lineContents[1])
+                            else:
                                 megaDict[lineContents[0]] = lineContents[1:]
                     if termString is not None:
                         if termString in line:
@@ -49,3 +61,13 @@ class bcSim:
         generated_particles = int(self.simDict['TS'][0])
 
         return r_sphere**2*pi*triggers/generated_particles
+
+    def passEres(self, alpha):
+
+        ed = np.array(self.simDict['ED']).astyp(np.float)
+        ec = np.array(self.simDict['EC']).astyp(np.float)
+        ns = np.array(self.simDict['NS']).astyp(np.float)
+        
+        tot = ed + ec + ns
+
+        return tot
