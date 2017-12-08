d55b7c5336ae7577856cc4859b5991e64d235836
Jeremy S. Perkins <jeremyshane@gmail.com>
2017-12-08 16:55:30 -0500

    Added the filename routines to utils.

--- MEGAlib/bin/utils.py
+++ MEGAlib/bin/utils.py
@@ -19,3 +19,26 @@ def setPath(desiredPath = "", desiredPathName = 'BURSTCUBE'):
     else:
         return 0
 
+def getFilenameFromDetails(details):
+    
+    filename =  "{}_{:.3f}keV_Cos{:.3f}".format(details['base'],
+                                                details['keV'],
+                                                details['Cos'])
+
+    return filename
+
+
+def getDetailsFromFilename(filename):
+
+    '''Function to get the energy and angle from a filename.
+    Really should be meta data.'''
+
+    details = {}
+    info = filename.split('_')
+    details['keV'] = float(info[1][:-3])
+
+    angle = info[2].split('.')
+    details['Cos'] = float("{}.{}".format(angle[0][3:], angle[1]))
+
+    return details
+
