06b2ccfbc63c2d02e9b332346ada6af3015118b6
Jeremy S. Perkins <jeremyshane@gmail.com>
2017-12-12 13:23:33 -0500

    Removed some old files.

--- MEGAlib/bin/execute_cosima.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-FILES=/Users/reginamcaputo/Desktop/BurstCube/Simulations/MEGAlib/source/FarFieldPointSource_test.source
-for f in $FILES
-do
-  echo "Processing $f file..."
-  # take action on each file. $f store current file name
-  cosima $f
-done
