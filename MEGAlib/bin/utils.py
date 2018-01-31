1a45614af8e1a64179fe71f73238b9e1271242d4
Regina Caputo <regina.caputo@nasa.gov>
2018-01-31 13:11:12 -0500

    adding trigger probabilities for sim files

--- MEGAlib/bin/utils.py
+++ MEGAlib/bin/utils.py
@@ -50,3 +50,59 @@ def getDetailsFromFilename(filename):
 
     return details
 
+def getTriggerProbability(htsimfile, test=False):
+
+    '''Takes a single simFile (from bcSim.simFiles(config.yaml)) and 
+    returns the probability of hitting in each detector '''
+
+    det_vol = 0 
+    det_vol1 = 0
+    det_vol2 = 0
+    det_vol3 = 0
+
+    actual = 0
+    if test: dotest=20
+    else: dotest=len(htsims)
+
+    siminfo=htsimfile.simDict['HTsim 4']
+    energy = htsimfile.energy
+    theta = htsimfile.theta
+    print energy, theta
+    
+    for i in range(0,dotest):
+        if isinstance(siminfo[i],list): 
+            actual+=1
+            if float(siminfo[i][0])> 0.:
+                if float(siminfo[i][1])>0.:
+                    det_vol1+=1
+                else:
+                    det_vol3+=1
+            else:
+                if float(siminfo[i][1])>0.:
+                    det_vol2+=1
+                else:
+                    det_vol+=1
+                     
+    prob_det_info=[energy, theta, det_vol1/float(actual), det_vol3/float(actual), det_vol/float(actual), det_vol2/float(actual)]
+
+                  
+
+    return prob_det_info
+    
+
+def getAllTriggerProbability(filelist, test=False):
+    
+    htsims=filelist.sims
+    
+    if test: dotest=1
+    else: dotest=len(htsims)
+
+    print "Analyzing", len(htsims), "files"
+
+    for j in range(dotest):
+        det_prob = getTriggerProbability(htsims[j], test=test)
+
+
+    return det_prob[2:4]
+        
+    
