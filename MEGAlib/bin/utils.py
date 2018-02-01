f67e95fbe14d739c7b93088060b5dce5ee430ddf
Regina Caputo <regina.caputo@nasa.gov>
2018-02-01 10:46:33 -0500

    checking in something that isn't quite working yet

--- MEGAlib/bin/utils.py
+++ MEGAlib/bin/utils.py
@@ -52,7 +52,52 @@ def getDetailsFromFilename(filename):
 
     return details
 
-def getTriggerProbability(htsimfile, test=False):
+#def getTriggerProbability(htsimfile, test=False):
+#
+#    """Takes a single simFile (from bcSim.simFiles(config.yaml)) and 
+#    returns the probability of hitting in each detector
+#
+#    Parameters
+#    ----------
+#    self : simFile 
+#    test : run a quick test over a limited number of events (20)
+#    
+#    Returns
+#    ----------
+#    prob_det_info : 1x6 numpy array containing information about the energy, angles and probability of 
+#    hitting a given detector
+#    
+#    """
+#
+#    det_vol = 0 
+#    det_vol1 = 0
+#    det_vol2 = 0
+#    det_vol3 = 0
+#
+#    energy = htsimfile.energy
+#    theta = htsimfile.theta
+#    hits = htsimfile.getHits()
+#
+#    actual = 0
+#    if test: dotest=20
+#    else: dotest=len(hits)
+#
+#    print "analyzing", len(hits), "events"
+#
+#    for i in range(0,dotest):
+#        actual+=1
+#        if hits[i][0]>0:
+#            if hits[i][1]>0: det_vol1+=1
+#            else: det_vol3+=1
+#        else:
+#            if hits[i][1]>0: det_vol2+=1
+#            else: det_vol+=1
+#                     
+#    prob_det_info=[energy, theta, det_vol1/float(actual), det_vol3/float(actual), det_vol/float(actual), det_vol2/float(actual)]
+#
+#    return prob_det_info
+
+def getTriggerProbability(htsimfile, num_det=4, test=False):
 
     """Takes a single simFile (from bcSim.simFiles(config.yaml)) and 
     returns the probability of hitting in each detector
@@ -69,36 +114,34 @@ def getTriggerProbability(htsimfile, test=False):
     
     """
 
-    det_vol = 0 
-    det_vol1 = 0
-    det_vol2 = 0
-    det_vol3 = 0
+    import numpy as np
+
+    det_vol=np.zeros(num_det)
+
+    print "test", det_vol
 
     energy = htsimfile.energy
     theta = htsimfile.theta
     hits = htsimfile.getHits()
 
-    actual = 0
+    actual = 1
     if test: dotest=20
     else: dotest=len(hits)
 
     print "analyzing", len(hits), "events"
 
-    for i in range(0,dotest):
-        actual+=1
-        if hits[i][0]>0:
-            if hits[i][1]>0: det_vol1+=1
-            else: det_vol3+=1
-        else:
-            if hits[i][1]>0: det_vol2+=1
-            else: det_vol+=1
+    for key, value in htsimfile.logDict.items():
+        print hits[key], value[1]
+        for i in range(num_det):
+            if str(i) in value[1]:
+                print i, value
                      
     prob_det_info=[energy, theta, det_vol1/float(actual), det_vol3/float(actual), det_vol/float(actual), det_vol2/float(actual)]
 
     return prob_det_info
     
 
-def getAllTriggerProbability(filelist, test=False):
+def getAllTriggerProbability(filelist, num_detectors=4, test=False):
 
     """Takes a bunch of simFiles (from bcSim.simFiles(config.yaml)) and 
     returns the probability of hitting in each detector
@@ -130,7 +173,7 @@ def getAllTriggerProbability(filelist, test=False):
     print "Analyzing", len(htsims), "files"
 
     for j in range(dotest):
-        holder=getTriggerProbability(htsims[j], test=test)
+        holder=getTriggerProbability(htsims[j], num_det=num_detectors, test=test)
         det_prob[j]= np.array([tuple(holder)], dtype=det_prob.dtype)
 
 
