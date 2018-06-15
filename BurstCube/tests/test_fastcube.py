0573f3f01b3ff39757113889aacda40c0662d715
Jeremy S. Perkins <jeremyshane@gmail.com>
2018-06-15 16:41:00 -0400

    Testing the detA's.

--- BurstCube/tests/test_fastcube.py
+++ BurstCube/tests/test_fastcube.py
@@ -1,41 +1,47 @@
 from BurstCube.NoahSim import fastcube
 from BurstCube.NoahSim import GRBgenerator
 import numpy as np
+from astropy.tests.helper import pytest
 
 
-def test_GRBgenerator():
-
-        testsim = GRBgenerator.Sky(1, 500)
-        
-        assert ((testsim.Ao == 500) & (testsim.pixels == 12))
-
-        
-def test_GRBgeneratorSourceAngs():
+@pytest.fixture(scope='module')
+def create_sky(request):
 
         testsim = GRBgenerator.Sky(1, 500)
 
-        result = [(0.84106867056793033, 0.78539816339744828),
-                  (0.84106867056793033, 2.3561944901923448),
-                  (0.84106867056793033, 3.926990816987241),
-                  (0.84106867056793033, 5.497787143782138),
-                  (1.5707963267948966, 0.0),
-                  (1.5707963267948966, 1.5707963267948966),
-                  (1.5707963267948966, 3.1415926535897931),
-                  (1.5707963267948966, 4.7123889803846897),
-                  (2.3005239830218631, 0.78539816339744828),
-                  (2.3005239830218631, 2.3561944901923448),
-                  (2.3005239830218631, 3.926990816987241),
-                  (2.3005239830218631, 5.497787143782138)]
+        return testsim
 
-        np.testing.assert_allclose(testsim.sourceangs, result, 1e-15)
 
-        
-def test_fastcube():
-        testsim = GRBgenerator.Sky(1, 500)
+@pytest.fixture(scope='module')
+def create_fastcube(request):
+
         testcube = fastcube.FastCube(1000, 45)
-        testresponse = testcube.response2GRB(testsim, samples=10,
-                                             test=True, talk=False)
+
+        return testcube
+
+
+def test_det():
+
+        tc = fastcube.FastCube(1000,45)
+        
+        result = [[0.78539816339744828, 0],
+                  [0.78539816339744828, 1.5707963267948966],
+                  [0.78539816339744828, 3.141592653589793],
+                  [0.78539816339744828, 4.71238898038469]]
+        
+        dets = [getattr(tc, 'det{}'.format(letter))
+                for letter in ['A', 'B', 'C', 'D']]
+                
+        np.testing.assert_allclose(dets, result, 1e-16)
+
+        
+
+# def test_fastcube():
+#        testsim = GRBgenerator.Sky(1, 500)
+#        testcube = fastcube.FastCube(1000, 45)
+#        testresponse = testcube.response2GRB(testsim, samples=10,
+#                                             test=True, talk=False)
 
         # leaving this a little more room for error
         # since I'm messing up one detector.
-        np.testing.assert_allclose(testresponse[0], 6.0, 1.0)
+#        np.testing.assert_allclose(testresponse[0], 6.0, 1.0)
